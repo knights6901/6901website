5a969e3c4dae5c818319980363545e79b28d6beb
eshaanatknights <eshaan@knights6901.org>
2026-04-26 00:44:57 -0500

    hopefully fixed 404 error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-// If deploying to GitHub Pages without a custom domain, uncomment and set these:
-// site: 'https://arjpeg.github.io',
-// base: '/6901website',
-
 export default defineConfig({
+  // For GitHub Pages deployment with custom domain
+  site: 'https://knights6901.org',
   output: 'static',
 });

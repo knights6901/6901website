989e0468b85e1c3a4bac81a83654df7bb0900ef0
eshaanatknights <eshaan@knights6901.org>
2026-04-26 20:58:44 -0500

    hopefully fix shit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // For GitHub Pages deployment with custom domain
   site: 'https://knights6901.org',
+  base: '/',
   output: 'static',
+  outDir: './dist',
 });
